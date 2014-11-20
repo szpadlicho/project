5a5191dc13359f73a4cb7f4486bac513470f8b57
DOM <szpadlicho@gmail.com>
2014-11-20 21:49:58 +0100

    .

--- interface/index.php
+++ interface/index.php
@@ -138,6 +138,27 @@
                 $(function() {
                     $( "#draggable2" ).draggable();
                 });
+                /*
+                $(function() {
+                    $( "#set div" ).draggable({ 
+                        stack: "#set div",
+                        stop: function(event, ui) {
+                            var pos_x = ui.offset.left;
+                            var pos_y = ui.offset.top;
+                            var need = ui.helper.data("need");
+
+                            //Do the ajax call to the server
+                            $.ajax({
+                                type: "POST",
+                                url: "your_php_script.php",
+                                data: { x: pos_x, y: pos_y, need_id: need}
+                            }).done(function( msg ) {
+                                alert( "Data Saved: " + msg );
+                            }); 
+                        }
+                    });
+                });
+                */
                 </script>
                 <div id="middle-ph">
                 <!--<img id="picture" src="../repo/data/src1.jpg" />-->
