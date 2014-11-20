fe908e1e29a5477b4c4738b77b1397ed733dd590
unknown <szpadlicho@op.pl>
2014-11-20 15:51:56 +0100

    menu and tools create

--- interface/index.php
+++ interface/index.php
@@ -12,6 +12,8 @@
         <link title="deafult" type="text/css" rel="stylesheet" href="css/reset.css" />
         <link title="deafult" type="text/css" rel="stylesheet" href="css/style.css" />
         <link title="deafult" type="text/css" rel="stylesheet" href="css/menu.css" />
+        <link title="deafult" type="text/css" rel="stylesheet" href="css/option.css" />
+        <link title="deafult" type="text/css" rel="stylesheet" href="css/tools.css" />
         
         <script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
         <script type="text/javascript" src="js/scrypt.js"></script>
@@ -30,6 +32,26 @@
         $(document).load(both);
         $(window).resize(both);
         $(document).ready(function(){
+            $( '#top-menu ul li ul li' ).click(function(){
+                //$( '#top-menu ul li ul li' ).removeClass( 'active' );
+                if ( $( this ).hasClass( 'active' ) ) {
+                    $( this ).removeClass( 'active' );
+                } else {
+                    $( this ).addClass( 'active' );
+                };
+                
+            });
+            $( '[id^="tool-"]' ).hide(); // hide the other divs
+            $( 'li[id^="button-"]' ).click(function(){
+                if ( $( '#tool-'+this.id.slice(7) ).hasClass( 't-show' ) ) {
+                    $( '#tool-'+this.id.slice(7) ).removeClass( 't-show' );
+                    $( '#tool-'+this.id.slice(7) ).hide('explode').addClass( 't-hide' );
+                } else {
+                    $( '#tool-'+this.id.slice(7) ).removeClass( 't-hide' );
+                    $( '#tool-'+this.id.slice(7) ).show('explode').addClass( 't-show' );;
+                }
+                //$( '#tool-'+this.id.slice(7) ).show('explode');
+            });
         });
         </script>
     </head>
@@ -40,18 +62,48 @@
                     <ul>
                         <li>Plik
                             <ul>
-                                <li>Open</li>
-                                <li>Close</li>
-                                <li>Reset</li>
+                                <li id="button-1">Open</li>
+                                <li id="button-2">Close</li>
+                                <li id="button-3">Reset</li>
                             </ul>
                         </li>
-                        <li>Edycja</li>
+                        <li>Widok
+                            <ul>
+                                <li id="button-4">Rozmiar</li>
+                                <li id="button-5">Czcionka</li>
+                                <li id="button-6">Napis</li>
+                            </ul>
+                        </li>
+                        <li>Edycja
+                            <ul>
+                                <li id="button-7">one</li>
+                                <li id="button-8">two</li>
+                                <li id="button-9">three</li>
+                            </ul>
+                        </li>
                         <li>Zapisz</li>
                         <li>Pobierz</li>
                     </ul>
                 </nav>
             </header>
-            <article></article><div></div>
+            <article>
+                <!--
+                <div id="left-option-ph">
+                    <div class="left-option"><span>Rozmiar</span></div>
+                    <div class="left-option"><span>Czcionka</span></div>
+                    <div class="left-option"><span>Napis</span></div>
+                </div>
+                -->
+                <div id="tool-1" class="tools">a</div>
+                <div id="tool-2" class="tools">b</div>
+                <div id="tool-3" class="tools">c</div>
+                <div id="tool-4" class="tools">d</div>
+                <div id="tool-5" class="tools">e</div>
+                <div id="tool-6" class="tools">f</div>
+                <div id="tool-7" class="tools">g</div>
+                <div id="tool-8" class="tools">h</div>
+                <div id="tool-9" class="tools">i</div>
+            </article>
         </section>
         <footer>
             <div id="count"></div>
